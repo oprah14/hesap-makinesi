5376366a12607bada558d015e99a3d745410578b
oprah14 <leventkingman@gmail.com>
2025-07-24 22:35:43 +0200

    update

--- hesap-mak.py
+++ hesap-mak.py
@@ -1,22 +1,25 @@
 import os
-def toplama(x, y):
+def addition(x, y):
     return x + y
 
-def cikarma(x, y):
+def substraction(x, y):
     return x - y
 
-def carpma(x, y):
+def multiplication(x, y):
     return x * y
 
-def bolme(x, y):
+def division(x, y):
     if y == 0:
         print("Bir sayi 0'a bölünemez!")
         return 0
     else:
         return x / y    
 
-def us_alma(x, y):
+def exponentiation(x, y):
     return x ** y
+
+x = 0
+y = 0
 while True:
 
     os.system('cls')
@@ -32,25 +35,25 @@ while True:
 
     sec= input("Lütfen yapmak istediğiniz işlemi seçiniz (1-5): ")
     if (sec == '1'):
-        a = float(input("Birinci sayiyi giriniz: "))
-        b = float(input("İkinci sayiyi giriniz: "))
-        print(f"Sonuç: {toplama(a, b)}")
+        x = float(input("Birinci sayiyi giriniz: "))
+        y = float(input("İkinci sayiyi giriniz: "))
+        print(f"Sonuç: {addition(x, y)}")
     elif (sec == '2'):
-        a = float(input("Birinci sayiyi giriniz: "))
-        b = float(input("İkinci sayiyi giriniz: "))    
-        print(f"Sonuç: {cikarma(a, b)}")
+        x = float(input("Birinci sayiyi giriniz: "))
+        y = float(input("İkinci sayiyi giriniz: "))
+        print(f"Sonuç: {substraction(x, y)}")
     elif (sec == '3'):
-        a = float(input("Birinci sayiyi giriniz: "))
-        b = float(input("İkinci sayiyi giriniz: "))
-        print(f"Sonuç: {carpma(a, b)}")
+        x = float(input("Birinci sayiyi giriniz: "))
+        y = float(input("İkinci sayiyi giriniz: "))
+        print(f"Sonuç: {multiplication(x, y)}")
     elif (sec == '4'):
-        a = float(input("Birinci sayiyi giriniz: "))
-        b = float(input("İkinci sayiyi giriniz: "))
-        print(f"Sonuç: {bolme(a, b)}")
+        x = float(input("Birinci sayiyi giriniz: "))
+        y = float(input("İkinci sayiyi giriniz: "))
+        print(f"Sonuç: {division(x, y)}")
     elif (sec == '5'):
-        a = float(input("Taban sayiyi giriniz: "))
-        b = float(input("Üs sayisini giriniz: "))
-        print(f"Sonuç: {us_alma(a, b)}")
+        x = float(input("Taban sayiyi giriniz: "))
+        y = float(input("Üs sayisini giriniz: "))
+        print(f"Sonuç: {exponentiation(x, y)}")
     else:
         print("Geçersiz seçim! Lütfen 1-5 arasında bir sayı giriniz.")
     input("Devam Etmek Istiyorsan ENTER'a Bas...")    
